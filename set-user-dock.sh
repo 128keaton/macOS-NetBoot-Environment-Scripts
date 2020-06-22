3f06b7ef2fda91ff65e4870d0abf23685fd700ad
Keaton Burleson <keaton.burleson@me.com>
2020-06-22 13:28:13 -0500

    Update set-user-dock.sh

--- set-user-dock.sh
+++ set-user-dock.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+sleep 30
+DOCKUTIL="/Library/Application Support/AutoNBI/Scripts/dockutil"
 
 # Delete everything from the dock and replace it with a specific selection of apps.
-./dockutil --remove all --no-restart
+"$DOCKUTIL" --remove all --no-restart
 
 # We add a delay so that the dock has time to initialize the removal
 sleep 2
 
 # Finally, add our applications
-./dockutil --add /Applications/Safari.app --no-restart
-./dockutil --add /System/Applications/Utilities/Terminal.app --no-restart
-./dockutil --add /Applications/Prime95.app --no-restart
-./dockutil --add /Applications/FurMark.app --no-restart
-./dockutil --add '/Applications' --view list 
+"$DOCKUTIL" --add /Applications/Safari.app --no-restart
+"$DOCKUTIL" --add /System/Applications/Utilities/Terminal.app --no-restart
+"$DOCKUTIL" --add /Applications/Prime95.app --no-restart
+"$DOCKUTIL" --add /Applications/FurMark.app --no-restart
+"$DOCKUTIL" --add '/Applications' --view list
