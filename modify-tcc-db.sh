ddf186a31c3ba54569d18816880f23e3a3c4777e
Keaton Burleson <keaton.burleson@me.com>
2020-06-24 22:46:04 -0500

    Update modify-tcc-db.sh

--- modify-tcc-db.sh
+++ modify-tcc-db.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-#Inserts code into TCC.db inside ~/Application Support/com.apple.TCC/TCC.db for OSAScript Accessibility 
-#Get Logged In User
-loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
-checkTCCdone()  {
-        if [ -e /Users/$LoggedInUser/Library/Preferences/TCCdone.txt ] ; then
+
+loggedInUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+
+checkTCCdone() {
+  if [ -e /Users/"$loggedInUser"/Library/Preferences/TCCdone.txt ]; then
     echo "TCC.db has been modified."
-        exit 0
-        fi
+    exit 0
+  fi
 }
 
 configureTCCset() {
-/usr/bin/sqlite3 /Users/$loggedInUser/Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','/usr/bin/osascript',1,1,1,NULL,NULL)"
-/usr/bin/touch /Users/$loggedInUser/Library/Preferences/TCCdone.txt
+  /usr/bin/sqlite3 /Users/"$loggedInUser"/Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','com.keatonburleson.macOS-Utilities',1,1,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL)"
+  /usr/bin/touch /Users/"$loggedInUser"/Library/Preferences/TCCdone.txt
 }
 
 checkTCCdone
